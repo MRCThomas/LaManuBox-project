3125a1ff72f58d02242cb1deb79b854e73765c82
Thomas Marchi <thomas.marchi@orange.fr>
2023-10-18 11:58:16 +0200

    fonction connexion et création de compte

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import CreateAccount from './components/Inscription-Connexion/createAccount';
+import Login from './components/Inscription-Connexion/login';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+    setShowCreateAccount(false);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {loggedIn ? (
+          <div>
+            <h1>Bienvenue, {localStorage.getItem('username')}!</h1>
+            <button onClick={handleLogout}>Déconnexion</button>
+          </div>
+        ) : (
+          showCreateAccount ? (
+            <CreateAccount onLogin={handleLogin} />
+          ) : (
+            <div>
+              <Login onLogin={handleLogin} />
+              <button onClick={() => setShowCreateAccount(true)}>Créer son compte</button>
+            </div>
+          )
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
