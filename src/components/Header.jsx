8a141760e5445167641a798da76074906fa1a429
Thomas Marchi <thomas.marchi@orange.fr>
2023-10-23 11:22:15 +0200

    commit header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import UserStateComponent from '../components/UserStateComponent'
+import { AuthContext } from '../context/AuthContext';
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    const authContext = useContext(AuthContext);
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <a href="id-de-la-section-boxes">Nos boxes</a>
+                    </li>
+                    {!authContext.userToken ? (
+                        <>
+                            <li>
+                                <NavLink to="login">Se connecter</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="sign-in">S'inscrire</NavLink>
+                            </li>
+                        </>
+                    ) : (
+                        <li><UserStateComponent /></li>
+                    )
+
+                    }
+
+                </ul>
+            </nav>
+        </header>
+    )
 }
